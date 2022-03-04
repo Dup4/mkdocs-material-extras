503187fd24141e347b625025cbcdf9bb2c0ef7db
Dup4 <lyuzhi.pan@gmail.com>
2022-03-04 13:38:54 +0800

    chore: pypi support

--- README.md
+++ README.md
@@ -4,19 +4,4 @@
 
 ```bash
 pip3 install -U -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
-
-yarn
 ```
-
-## Build
-
-```bash
-yarn build
-```
-
-## Serve
-
-```bash
-yarn serve
-```
-
---
+++ mkdocs-material-extras/__init__.py
@@ -0,0 +1,35 @@
+"""Mkdocs material Custom."""
+from .__meta__ import __version__, __version_info__  # noqa: F401
+import glob
+import os
+import mkdocs.plugins
+
+RESOURCE_PATH = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), 'theme')
+
+
+class MkdocsMaterialExtras(mkdocs.plugins.BasePlugin):
+    """Plugin to add custom assets to Material theme."""
+
+    def on_config(self, config, **kwargs):
+        """Add additional assets."""
+
+        # Add our theme resources to the theme path.
+        config['theme'].dirs.insert(0, RESOURCE_PATH)
+
+        base_path = RESOURCE_PATH.replace('\\', '/') + '/'
+
+        # Add our extra styles and JavaScript to be included in the template.
+        extras = set(config['extra_css'])
+        for f in glob.glob(base_path + '**/*.css', recursive=True):
+            name = f.replace('\\', '/').replace(base_path, '')
+            if name not in extras:
+                config['extra_css'].append(name)
+
+        extras = set(config['extra_javascript'])
+        for f in glob.glob(base_path + '**/extra*.js', recursive=True):
+            name = f.replace('\\', '/').replace(base_path, '')
+            if name not in extras:
+                config['extra_javascript'].append(name)
+
+        return config
