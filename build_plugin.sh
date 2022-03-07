7005f1b6367f540aac00c8985c49313530c7efca
Dup4 <lyuzhi.pan@gmail.com>
2022-03-07 12:36:44 +0800

    chore: get version with build plugin

--- build_plugin.sh
+++ build_plugin.sh
@@ -6,8 +6,10 @@ set -ex
 
 TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
+VERSION_ID=$(grep <"${TOP_DIR}/package.json" version | sed 's|  \"version\": \"||g' | sed 's|\",||g' | tr -d ' ')
+
 python3 setup.py sdist
 
-TARGET=$(ls "${TOP_DIR}"/dist/mkdocs-material-extras*.tar.gz)
+TARGET="${TOP_DIR}/dist/mkdocs-material-extras-${VERSION_ID}.tar.gz"
 
 pip install "${TARGET}"
